8221618b8c123b2e26fc7fe57364561418686313
FullStackWizardry <FullStackWizardry@proton.me>
2026-02-16 21:24:43 +0330

    updated!
    Added a script to reverse a given number.

--- reverse-number.py
+++ reverse-number.py
@@ -1,1 +1,10 @@
 
+"""
+Reverse a Number From input and print the reversed number.
+Example:
+Input: 12345
+Output: 54321
+"""
+num = int(input("Enter a number: "))
+rev = str(num)[::-1]
+print("Reversed Number:", rev)
